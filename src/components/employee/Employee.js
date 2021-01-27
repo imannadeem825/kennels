6af11fa32b0b66b5a3b883db1eb95e744b135169
Iman Nadeem <iman.nadeem.825@gmail.com>
2021-01-27 13:46:54 -0600

    chapter 7 complete

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Employee.css"
 
-export const Employee = () => (
+export const Employee = ({ employee }) => (
     <section className="employee">
-        <h3 className="employee__name">Emma Beaton</h3>
-        <div className="employee__location">200 Main Street</div>
+        <h3 className="employee__name">{employee.name}</h3>
+        <address className="location__address">{employee.location.name}</address>
     </section>
 )
